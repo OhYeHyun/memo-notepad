c1dedc5c9367a832082d46f93983d554b75ef0d7
OhYeHyun <ohyhohyl@gmail.com>
2025-04-07 16:29:27 +0900

    Initial commit

---
+++ src/test/java/com/yehyun/memo/notepad/NotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yehyun.memo.notepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
