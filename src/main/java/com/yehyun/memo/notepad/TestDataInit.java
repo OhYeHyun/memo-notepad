0eb9402a56039bde1ff3c509c68c82f2d49be880
OhYeHyun <ohyhohyl@gmail.com>
2025-04-07 19:01:53 +0900

    테스트 데이터 추가

---
+++ src/main/java/com/yehyun/memo/notepad/TestDataInit.java
@@ -0,0 +1,20 @@
+package com.yehyun.memo.notepad;
+
+import com.yehyun.memo.notepad.domain.Memo;
+import com.yehyun.memo.notepad.repository.MemoRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final MemoRepository memoRepository;
+
+    @PostConstruct
+    public void init() {
+        memoRepository.save(new Memo("테스트 1"));
+        memoRepository.save(new Memo("테스트 2"));
+    }
+}
