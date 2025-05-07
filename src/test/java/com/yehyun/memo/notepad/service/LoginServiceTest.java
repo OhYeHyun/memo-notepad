766ef5f03faf5e1a2aa76804f85400b84f1535b4
OhYeHyun <ohyhohyl@gmail.com>
2025-05-07 22:42:28 +0900

    refactor: 배포, 로컬, 테스트 프로파일 별 환경 설정 분리

--- src/test/java/com/yehyun/memo/notepad/service/LoginServiceTest.java
+++ src/test/java/com/yehyun/memo/notepad/service/LoginServiceTest.java
@@ -4,6 +4,7 @@ import com.yehyun.memo.notepad.domain.member.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 class LoginServiceTest {
 
     @Autowired
