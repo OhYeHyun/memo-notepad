696f3c2e9efa2e457e62959677e1ac73818c82f7
OhYeHyun <ohyhohyl@gmail.com>
2025-05-21 22:37:05 +0900

    feat: 게스트로 사용하다가 기존 회원 로그인 시 메모 이전 기능

--- src/main/java/com/yehyun/memo/notepad/security/handler/CustomSuccessHandler.java
+++ src/main/java/com/yehyun/memo/notepad/security/handler/CustomSuccessHandler.java
@@ -1,7 +1,10 @@
 package com.yehyun.memo.notepad.security.handler;
 
+import com.yehyun.memo.notepad.domain.member.Member;
 import com.yehyun.memo.notepad.security.dto.PrincipalMember;
 import com.yehyun.memo.notepad.security.jwt.JwtLoginSuccessProcessor;
+import com.yehyun.memo.notepad.security.jwt.JwtRequestParser;
+import com.yehyun.memo.notepad.service.MemoService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -16,10 +19,20 @@ import java.io.IOException;
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JwtLoginSuccessProcessor jwtLoginSuccessProcessor;
+    private final JwtRequestParser jwtRequestParser;
+    private final MemoService memoService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         PrincipalMember principalMember = (PrincipalMember) authentication.getPrincipal();
+
+        String guestToken = jwtRequestParser.extractTokenFromCookies(request.getCookies());
+
+        if (guestToken != null) {
+            Member guest = jwtRequestParser.createMemberFromToken(guestToken);
+            memoService.updateWriterId(guest.getLoginId(), principalMember.getName());
+        }
+
         jwtLoginSuccessProcessor.processSuccess(response, principalMember);
         getRedirectStrategy().sendRedirect(request, response, "/");
     }
