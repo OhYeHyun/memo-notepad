2d2a8f105132af383bff9c4222c2fbe371d941e6
OhYeHyun <ohyhohyl@gmail.com>
2025-05-26 18:35:46 +0900

    feat: RedisService 추가 - 키 refresh:loginId, 값 refreshToken

---
+++ src/main/java/com/yehyun/memo/notepad/security/service/RedisService.java
@@ -0,0 +1,26 @@
+package com.yehyun.memo.notepad.security.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public String getRefreshTokenByLoginId(String loginId) {
+        return redisTemplate.opsForValue().get("refresh:" + loginId);
+    }
+
+    public void saveRefreshToken(String loginId, String refreshToken, Duration expiration) {
+        redisTemplate.opsForValue().set("refresh:" + loginId, refreshToken, expiration);
+    }
+
+    public void deleteRefreshToken(String loginId) {
+        redisTemplate.delete("refresh:" + loginId);
+    }
+}
